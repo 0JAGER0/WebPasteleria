9372eee59cde2e070e32458b67261dc71eaa2375
aljimenez-atr <al.jimenez@duocuc.cl>
2021-05-29 17:09:11 -0400

    urls update

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Start, nosotros , productos
+from .views import Start, nosotros , productos , contacto
 
 urlpatterns = [
     path('',Start, name="Start"),
     path('',nosotros,name="nosotros"),
-    path('',productos,name="productos")
+    path('',productos,name="productos"),
+    path('',contacto,name="contacto")
 ]
