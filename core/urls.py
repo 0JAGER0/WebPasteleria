b63ba899ef4bfe10bbcd4d48be8a1e8a3153da1b
aljimenez-atr <al.jimenez@duocuc.cl>
2021-05-29 17:45:26 -0400

    Union html con css en django

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,19 @@
+from os import name
 from django.urls import path
-from .views import Start, nosotros , productos , contacto , registro
+from .views import Start, nosotros , productos , contacto , registro , fomularioContac , operaBlanca , merengueframbuesa, login , hojaRasca ,HojaldreManjar , Cassatta , BiscochoFrambuesa
 
 urlpatterns = [
     path('',Start, name="Start"),
     path('',nosotros,name="nosotros"),
     path('',productos,name="productos"),
     path('',contacto,name="contacto"),
-    path('',registro,name="registro")
+    path('',registro,name="registro"),
+    path('',fomularioContac,name="formularioContac"),
+    path('',operaBlanca,name="operaBlanca"),
+    path('',merengueframbuesa,name="merengue-frambuesa"),
+    path('',login,name="login"),
+    path('',hojaRasca,name="hojaRasca"),
+    path('',HojaldreManjar,name="Hojaldre-Manjar"),
+    path('',Cassatta,name="Cassatta"),
+    path('',BiscochoFrambuesa,name="Biscocho-Frambuesa")
 ]
--- core/views.py
+++ core/views.py
@@ -16,3 +16,29 @@ def contacto(request):
 
 def fomularioContac(request):
     return render(request,'core/fomularioContac.html')
+
+def registro(request):
+    return render(request,'core/REGISTRO.HTML')
+
+
+def operaBlanca(request):
+    return render(request,'core/operaBlanca.html')
+
+def merengueframbuesa(request):
+    return render(request,'core/merengue-frambuesa.html')
+
+def login(request):
+    return render(request,'core/LOGIN.HTML')
+
+def hojaRasca(request):
+    return render(request,'core/hojaRasca.html')
+
+def HojaldreManjar(request):
+    return render(request,'core/Hojaldre_Manjar.html')
+
+def Cassatta(request):
+    return render(request,'core/Cassatta.html')
+
+
+def BiscochoFrambuesa(request):
+    return render(request,'core/Biscocho-Frambuesa.html')
