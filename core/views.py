2de7d36162858366f8923a1bef1bdcfaf0f297f5
yurivKmirano <di.uribe@duocuc.cl>
2021-06-11 19:56:30 -0400

    modificaciones

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from core.forms import FormularioForm,RegistroForm, listadoTortasForm
 from django.shortcuts import redirect, render
 from .models import formulario, registro, listadoTortas
+from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpRequest
 
 
 # Create your views here.
@@ -25,6 +28,13 @@ def formularioContac(request):
     return render(request,'core/formularioContac.html')
 
 def registros(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login/')
+    else:
+        form = UserCreationForm()
 
     return render(request,'core/registros.html')
 
