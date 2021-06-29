e2ae2548696baeabeeba76aa815045399c817837
aljimenez-atr <al.jimenez@duocuc.cl>
2021-06-29 14:23:56 -0400

    Implementacion urls de autentificacion contraseña

--- api_clase/urls.py
+++ api_clase/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import apiTortas,torta
+from .viewsLogin import login
 
 urlpatterns = [
     path('tortas/',apiTortas, name="tortas"),
-    path('torta/<pk>',torta, name="torta")
+    path('torta/<pk>',torta, name="torta"),
+    path('login/', login, name="login"),
 ]
